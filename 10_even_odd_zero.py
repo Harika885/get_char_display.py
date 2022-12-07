062fa1d1f94508864b747a459c548ee3c24d732c
Harika885 <118706847+Harika885@users.noreply.github.com>
2022-12-07 15:02:41 +0400

    Add files via upload

---
+++ 10_even_odd_zero.py
@@ -0,0 +1,4 @@
+n=int(input("Enter any Positive Number:"))
+if(n==0): print("The user entered {} Number".format)
+elif(n%2==0):print("The Number {} is Even Nuber".format(n))
+else:print("The number {} is odd Number".format(n))
